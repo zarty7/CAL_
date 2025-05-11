62b002718e247db77416ce08fcb0a4a3d31acb7f
CXuen <cxuen513@gmail.com>
2025-05-11 15:18:37 +0800

    V0.5 背景图片转换为MP4格式

---
+++ Webpage/src/get_token.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_access_token(api_key, secret_key):
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": api_key,
+        "client_secret": secret_key
+    }
+    response = requests.post(url, data=data)
+    return response.json().get("access_token")
+
+# 使用示例
+access_token = get_access_token("BvUsRWMK3CnFQdjGbrwCl9RF", "pL7Mao1qOlQvPcxRo89lkcD7KqE8rr0T") # 你的AK SK
+print("获取到的Access Token:", access_token)
+
+
+
+# Token: 24.1c6c3a889c7f623cf97526d2c3ae2d7c.2592000.1747633589.282335-118555727
